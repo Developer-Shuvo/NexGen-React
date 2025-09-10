99826c965ddc67e935b6332220b4bdb9bfe58887
SHUVO <im.computer.engineeer@gamil.com>
2025-09-10 20:03:08 +0600

    Hero section Ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import Navbar from "./assets/Components/Navbar/Navbar";
 function App() {
   return (
     <>
-      <div className="max-w-[1440px] mx-auto">
-        <Navbar />
-        <Hero />
+      <div
+      // This is the all page background color and pattern
+        className="flex justify-center w-[100%] absolute top-0 z-[-2] min-h-[100vh] h-auto bg-[#000000] 
+           bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] 
+           bg-[size:20px_20px]"
+      >
+        <div className="w-[1500px] relative">
+          <Navbar />
+          <Hero />
+          
+        </div>
       </div>
     </>
   );
